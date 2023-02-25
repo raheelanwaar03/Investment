1e93a5c7e67549f9ebeef7071f532dae98f15c19
Raheel Anwaar <raheelanwaar0316@gmail.com>
2023-02-25 16:34:45 +0500

    table will responsive

--- resources/views/user/work/team.blade.php
+++ resources/views/user/work/team.blade.php
@@ -78,7 +78,7 @@
                 <div class="col-md-12">
                     <h2 class="text-center">My all team members</h2>
                     <hr>
-                    <table id="myTable" class="table table-bordered">
+                    <table id="myTable" class="table table-responsive table-bordered">
                         <thead>
                             <tr class="">
                                 <th>Name</th>
