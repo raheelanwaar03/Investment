d6c38406770dee94e0dc6be1ef316ccff111cd10
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-03-30 23:52:46 +0500

    landing page improved

--- resources/views/LandingPage/welcome.blade.php
+++ resources/views/LandingPage/welcome.blade.php
@@ -15,7 +15,8 @@
                                         <p>Work at your part time / full time and money from online job,Data entry jobs,Form
                                             filling jobs.Wrok from your PC,Laptop or Mobile.</p>
                                         <div class="banner-readmore">
-                                            <a href="{{route('register')}}" class="button banner-btn" href="#">Register Now</a>
+                                            <a href="{{ route('register') }}" class="button banner-btn"
+                                                href="#">Register Now</a>
                                         </div>
                                     </div>
                                 </div>
@@ -36,7 +37,8 @@
                                         <p>Work at your part time / full time and money from online job,Data entry jobs,Form
                                             filling jobs.Wrok from your PC,Laptop or Mobile.</p>
                                         <div class="banner-readmore">
-                                            <a href="{{route('register')}}" class="button banner-btn" href="#">Register Now</a>
+                                            <a href="{{ route('register') }}" class="button banner-btn"
+                                                href="#">Register Now</a>
                                         </div>
                                     </div>
                                 </div>
@@ -56,67 +58,6 @@
                         <h2>Our Features</h2>
                         <p>Browse jobs from your interested sector.
                         </p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-item">
-                        <div class="service-text">
-                            <h4>Data Entry</h4>
-                            <p>You can do simple data entry jobs at your home.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-item">
-                        <div class="service-text">
-                            <h4>Typing Jobs</h4>
-                            <p>You can do simple typing jobs and you will be earn well!</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-item">
-                        <div class="service-text">
-                            <h4>Simple Task</h4>
-                            <p>We will give you simple task.That you have to daily for your daily earning</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-item">
-                        <div class="service-text">
-                            <h4>Refer Friend</h4>
-                            <p>You can also earn from our referal program by inviting your friends</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-item">
-                        <div class="service-text">
-                            <h4>Easy Widthraw</h4>
-                            <p>You can simply widthraw your earning in your easypaisa or jazzcash account.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-item">
-                        <div class="service-text">
-                            <h4>Contact us</h4>
-                            <p>You can contact us we are here to help you 24/7.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="service-area pt-120 pb-105">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-                    <div class="section-title text-center mb-55">
-                        <h2>About Us</h2>
                     </div>
                 </div>
             </div>
@@ -233,6 +174,40 @@
                         <div class="service-text">
                             <h4>Changes</h4>
                             <p>Update your Profile adn job preferences at anytime.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="service-area pt-120 pb-105">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
+                    <div class="section-title text-center mb-55">
+                        <h2>About Us</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="single-item">
+                        <div class="service-text">
+                            <h4>Our Vision</h4>
+                            <p>We can give opportunities for you to earn extra money from home, which is also global trend
+                                during the pandemic. You do not need to go out or go to the office, the office is crowded,
+                                you can work directly with us at home adn can spend more time with your family.</p>
+                        </div>
+                        <div class="service-text">
+                            <h4>Benifits</h4>
+                            <ul>
+                                <li>flexible working schedule. Data entry jobs offer a bigger advantage of working according to your specifications.</li>
+                                <li>Payment against entry.</li>
+                                <li>Low level of stress.</li>
+                                <li>Improves Skils.</li>
+                                <li>Improves communication skills.</li>
+                                <li>The process to Identify fraudulent Data Entry.</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
