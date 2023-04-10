41b792630ccb59e59edf7afb7fe546a7543e58f1
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-04-10 21:54:48 +0500

    testing

--- database/migrations/2023_03_26_213556_create_type_texts_table.php
+++ database/migrations/2023_03_26_213556_create_type_texts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('type_texts', function (Blueprint $table) {
             $table->id();
-            $table->text('user_text');
+            $table->text('user_text',1000)->change();
             $table->timestamps();
         });
     }
