eb8ba998d25fc723ddbde901202585af9f8906bb
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-05-28 20:56:31 +0500

    Working on admin side

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,23 +1,46 @@
-<div class="footer">
-    <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="https://www.fiyver.com/"
-                target="_blank">{{ env('APP_NAME') }}</a> 2019</p>
-    </div>
-</div>
-</div>
+<script src="{{ asset('admin/js/jquery1-3.4.1.min.js') }}"></script>
 
-{{-- js files --}}
+<script src="{{ asset('admin/js/popper1.min.js') }}"></script>
 
-<script src="{{asset('admin/public/vendor/global/global.min.js')}}" type="text/javascript"></script>
-    <script src="{{asset('admin/public/vendor/jquery-nice-select/js/jquery.nice-select.min.js')}}" type="text/javascript"></script>
-    <script src="{{asset('admin/public/vendor/chart.js/Chart.bundle.min.js')}}" type="text/javascript"></script>
-    <script src="{{asset('admin/public/vendor/apexchart/apexchart.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('admin/public/vendor/datatables/js/jquery.dataTables.min.js') }}" type="text/javascript"></script>
-    <script src="{{asset('admin/public/js/plugins-init/datatables.init.js')}}" type="text/javascript"></script>
-    <script src="{{asset('admin/public/js/custom.min.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('admin/public/js/deznav-init.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('admin/public/js/demo.js') }}" type="text/javascript"></script>
+<script src="{{ asset('admin/js/bootstrap1.min.js') }}"></script>
 
+<script src="{{ asset('admin/js/metisMenu.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/count_up/jquery.waypoints.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/chartlist/Chart.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/count_up/jquery.counterup.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/swiper_slider/js/swiper.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/niceselect/js/jquery.nice-select.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/owl_carousel/js/owl.carousel.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/gijgo/gijgo.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/datatable/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/buttons.flash.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/jszip.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/pdfmake.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/vfs_fonts.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('admin/vendors/datatable/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('admin/js/chart.min.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/progressbar/jquery.barfiller.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/tagsinput/tagsinput.js') }}"></script>
+
+<script src="{{ asset('admin/vendors/text_editor/summernote-bs4.js') }}"></script>
+<script src="{{ asset('admin/vendors/apex_chart/apexcharts.js') }}"></script>
+
+<script src="{{ asset('admin/js/custom.js') }}"></script>
+<script src="{{ asset('admin/vendors/apex_chart/bar_active_1.js') }}"></script>
+<script src="{{ asset('admin/vendors/apex_chart/apex_chart_list.js') }}"></script>
 </body>
 
 </html>
--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1,147 +1,119 @@
 <!DOCTYPE html>
-<html lang="en">
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<html lang="">
+
+{{-- This website is developed by Raheel Anwaar --}}
+{{-- Contact Me : raheelanwaar28@gmail.com --}}
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="csrf-token" content="9lQReAOP7TYMbm5nNnK5b7NtKpmZuMGXAIriNN7M">
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
-    <meta name="robots" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Some description for the page" />
-    <meta property="og:title" content="Lezato : Restaurant Admin Laravel Template" />
-    <meta property="og:description" content="Lezato | Dashboard" />
-    <meta property="og:image" content="../social-image.html" />
-    <meta name="format-detection" content="telephone=no">
-    <title>Admin | Dashboard</title>
-    {{-- css --}}
-    <link href="{{ asset('admin/public/vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link href="{{ asset('admin/public/vendor/jquery-nice-select/css/nice-select.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link href="{{ asset('admin/public/css/style.css') }}" rel="stylesheet" type="text/css" />
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Admin Dashboard</title>
+    <link rel="icon" href="{{ asset('admin/img/logo.png') }}" type="image/png">
+
+    <link rel="stylesheet" href="{{ asset('admin/css/bootstrap1.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/themefy_icon/themify-icons.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/swiper_slider/css/swiper.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/select2/css/select2.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/niceselect/css/nice-select.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/owl_carousel/css/owl.carousel.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/gijgo/gijgo.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/font_awesome/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/vendors/tagsinput/tagsinput.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/jquery.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/responsive.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/buttons.dataTables.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/text_editor/summernote-bs4.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/morris/morris.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/material_icon/material-icons.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/css/metisMenu.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('admin/css/style1.css') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/css/colors/default.css') }}" id="colorSkinCSS">
 </head>
 
-<body>
+<body class="crm_body_bg">
 
-    <div id="preloader">
-        <div class="gooey">
-            <span class="dot"></span>
-            <div class="dots">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-    </div>
-    <div id="main-wrapper">
-        <div class="nav-header">
-            <a href="{{ route('LandingPage') }}" class="brand-logo">
-                <img src="{{ asset('admin/public/images/logo.png') }}" height="60px" width="188px" alt="logo">
-            </a>
-            <div class="nav-control">
-                <div class="hamburger">
-                    <span class="line"></span><span class="line"></span><span class="line"></span>
-                </div>
-            </div>
-        </div>
-        <div class="header">
-            <div class="header-content">
-                <nav class="navbar navbar-expand">
-                    <div class="collapse navbar-collapse justify-content-between">
-                        <div class="header-left">
-                            <div class="nav-item">
-                                <div class="input-group search-area">
-                                    <input type="text" class="form-control" placeholder="Find something here......">
-                                    <span class="input-group-text"><a href="javascript:void(0)"><i
-                                                class="flaticon-381-search-2"></i></a></span>
-                                </div>
-                            </div>
-                        </div>
-                        <ul class="navbar-nav header-right">
-                            <li class="nav-item recipe">
-                                <form action="{{ route('logout') }}" method="Post">
-                                    @csrf
-                                    <button class="btn btn-primary btn-rounded">Logout</button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
-        {{-- links --}}
 
-        <div class="deznav">
-            <div class="deznav-scroll">
-                <ul class="metismenu" id="menu">
-                    <li><a class="has-arrow ai-icon" href="{{ route('Admin.Dashboard') }}" aria-expanded="false">
-                            <i class="fa fa-home"></i>
-                            <span class="nav-text">Dashboard</span>
-                        </a>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="ti-user"></i>
-                            <span class="nav-text">User's</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="{{ route('Admin.All.Users') }}">All User's</a></li>
-                            <li><a href="{{ route('Admin.Pending.Users') }}">Pending User's</a></li>
-                            <li><a href="{{ route('Admin.Approved.Users') }}">Approved User's</a></li>
-                            <li><a href="{{ route('Admin.Rejected.Users') }}">Rejected User's</a></li>
-                            <li><a href="{{ route('Admin.Today.Approved.User') }}">Today's Approved Users</a></li>
-                            <li><a href="{{ route('Admin.Vistors') }}">Today's Vistors</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="flaticon-041-graph"></i>
-                            <span class="nav-text">Settings</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="{{ route('Admin.Setting.index') }}">Add Referal limit</a></li>
-                            <li><a href="{{ route('Admin.Level.View') }}">Level setting</a></li>
-                            <li><a href="{{ route('Admin.Easypaisa.index') }}">Payment Page Details</a></li>
-                            <li><a href="{{ route('Admin.Add.Verification.Details') }}">Verification Page Text</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="flaticon-043-menu"></i>
-                            <span class="nav-text">Daily Task</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="{{ route('Admin.Product.create') }}">Add New Task</a></li>
-                            <li><a href="{{ route('Admin.Product.index') }}">All Tasks</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="flaticon-072-printer"></i>
-                            <span class="nav-text">Widthraw Request</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="{{ route('Admin.All.Widthraw.Requests') }}">All Widthraw Request</a></li>
-                            <li><a href="{{ route('Admin.Approved.Widthraw') }}">Approved Widthraw</a></li>
-                            <li><a href="{{ route('Admin.Rejected.Widthraw') }}">Rejected Widthraw</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                            <i class="ti-user"></i>
-                            <span class="nav-text">Profile</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="{{ route('profile.edit') }}">Edit Profile</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <form action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button class="btn btn-primary">Logout</button>
-                        </form>
-                    </li>
+
+    <nav class="sidebar">
+        <div class="logo d-flex justify-content-between">
+            <a href="{{ route('Admin.Dashboard') }}"><img src="{{ asset('admin/img/logo.png') }}" alt=""></a>
+            <div class="sidebar_close_icon d-lg-none">
+                <i class="ti-close"></i>
+            </div>
+        </div>
+        <ul id="sidebar_menu">
+            <li class="side_menu_title">
+                <span>Dashboard</span>
+            </li>
+            <li class="mm-active">
+                <a class="has-arrow" href="{{ route('Admin.Dashboard') }}" aria-expanded="false">
+
+                    <img src="{{ asset('admin/img/menu-icon/1.svg') }}" alt="">
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="side_menu_title">
+                <span>Users</span>
+            </li>
+            <li class="">
+                <ul>
+                    <li><a href="{{ route('Admin.All.Users') }}">All Users</a></li>
+                    <li><a href="{{ route('Admin.Pending.Users') }}">Pending Users</a></li>
+                    <li><a href="{{ route('Admin.Approved.Users') }}">Approved Users</a></li>
+                    <li><a href="{{ route('Admin.Rejected.Users') }}">Rejected Users</a></li>
+                    <li><a href="{{ route('Admin.Today.Approved.User') }}">Today's Approved</a></li>
+                    <li><a href="{{ route('Admin.Vistors') }}">Today's Vistors</a></li>
                 </ul>
-                <div class="copyright">
-                    <p><strong>{{ env('APP_NAME') }} coypright</strong> © 2019 All Rights Reserved</p>
-                </div>
-            </div>
-        </div>
+            </li>
+            <li class="side_menu_title">
+                <span>Settings</span>
+            </li>
+            <li class="">
+                <ul>
+                    <li><a href="{{ route('Admin.Setting.index') }}">Add Referal limit</a></li>
+                    <li><a href="{{ route('Admin.Level.View') }}">Level setting</a></li>
+                    <li><a href="{{ route('Admin.Easypaisa.index') }}">Payment Page Details</a></li>
+                    <li><a href="{{ route('Admin.Add.Verification.Details') }}">Verification Page Text</a></li>
+                </ul>
+            </li>
+            <li class="side_menu_title">
+                <span>Daily Video</span>
+            </li>
+            <li class="">
+                <ul>
+                    <li><a href="{{ route('Admin.Product.create') }}">Add New Task</a></li>
+                    <li><a href="{{ route('Admin.Product.index') }}">All Tasks</a></li>
+                </ul>
+            </li>
+            <li class="side_menu_title">
+                <span>Widthraw</span>
+            </li>
+            <li class="">
+                <ul>
+                    <li><a href="{{ route('Admin.All.Widthraw.Requests') }}">All Widthraw Request</a></li>
+                    <li><a href="{{ route('Admin.Approved.Widthraw') }}">Approved Widthraw</a></li>
+                    <li><a href="{{ route('Admin.Rejected.Widthraw') }}">Rejected Widthraw</a></li>
+                </ul>
+            </li>
+            <li class="ml-3">
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn btn-primary">Logout</button>
+                </form>
+            </li>
+        </ul>
+    </nav>
