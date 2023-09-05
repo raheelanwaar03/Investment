c4748774f4d61ae34b9254f5c85c111ec1bbf60c
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-09-05 14:01:06 +0500

    no change

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->decimal('balance',3,2)->default(0);
+            $table->integer('balance')->default(0);
             $table->string('referal')->default('default');
             $table->string('email')->unique();
             $table->string('plan');
